5f5f17c9d8ea5a27ec38c35896b58230d979106d
Andres <andres.gonzalez2392@gmail.com>
2022-08-09 10:20:30 -0400

    Dashboard Complete - JSON Read
    Dashboard Complete - JSON Read

---
+++ js/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '34244',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },    
+    {
+    productName: 'Mini Drone',
+    productNumber: '34244',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {
+    productName: 'Big Mini Drone',
+    productNumber: '5555',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+    },
+    {
+    productName: 'MacBook Pro',
+    productNumber: '34244',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '34244',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '34244',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+    },
+    {
+    productName: 'GPS 4K Drone',
+    productNumber: '222822',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+    },
+]
